6022216957b49252c933545581d49c2a2b1bdfe9
jdluques <juan.luque@utec.edu.pe>
2025-05-06 21:16:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-host_name = "172.31.31.181" # IPv4 privada de "MV Bases de Datos"
+host_name = "172.31.29.213" # IPv4 privada de "MV Bases de Datos"
 port_number = "8005"
 user_name = "root"
 password_db = "utec"
